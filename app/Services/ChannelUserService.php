11f584483246d7f1e10b6be512c29f7d55735688
Ahmed Oraby <01205082343ahmed@gmail.com>
2026-04-18 23:07:00 +0200

    edits

--- app/Services/ChannelUserService.php
+++ app/Services/ChannelUserService.php
@@ -19,13 +19,13 @@ class ChannelUserService extends Service
     {
         try {
             return Channel::where([
-                        'user_id'       => auth()->id(),
-                        'level'         => ChannelLevel::TEAM,
-                        'is_archived'   => YesOrNo::NO
-                    ])
-                    ->with(['members', 'communities', 'communities.subCommunities'])
-                    ->orderBy('updated_at', 'desc')
-                    ->paginate(self::PERPAGE);
+                'user_id' => auth()->id(),
+                'level' => ChannelLevel::TEAM,
+                'is_archived' => YesOrNo::NO
+            ])
+                ->with(['members', 'communities', 'communities.subCommunities'])
+                ->orderBy('updated_at', 'desc')
+                ->paginate(self::PERPAGE);
         } catch (Exception $e) {
             Log::error('Error while getting channels for authenticated user that his in it as admin', ['error' => $e->getMessage(), 'trace' => $e->__toString()]);
             return false;
@@ -38,52 +38,52 @@ class ChannelUserService extends Service
             $teamsJoinedIn = ChannelMember::where('member_group', '!=', ChannelGroup::ADMIN)
                 ->where('member_id', auth()->id())
                 ->pluck('team_id');
-    
+
             $teamsWithJoinedSubcommunities = Channel::whereHas('communities.subCommunities.members', function ($query) {
                 $query->where('member_id', auth()->id())->where('member_group', '!=', ChannelGroup::ADMIN);
             })->pluck('id');
-    
+
             $teamsToInclude = $teamsJoinedIn->merge($teamsWithJoinedSubcommunities);
-    
+
             // Retrieve channels and apply filters
             $channels = Channel::where([
-                'level'       => ChannelLevel::TEAM,
+                'level' => ChannelLevel::TEAM,
                 'is_archived' => YesOrNo::NO,
             ])
-            ->whereIn('id', $teamsToInclude)
-            ->with(['members', 'communities', 'communities.subCommunities'])
-            ->orderBy('updated_at', 'desc')
-            ->get()
-            ->filter(function ($channel) {
-                $isJoined = ChannelMember::where('member_id', auth()->id())
-                    ->where('channel_id', $channel->id)
-                    ->exists();
-                
-                return $isJoined || $channel->communities->count() > 0;
-            });
-    
+                ->whereIn('id', $teamsToInclude)
+                ->with(['members', 'communities', 'communities.subCommunities'])
+                ->orderBy('updated_at', 'desc')
+                ->get()
+                ->filter(function ($channel) {
+                    $isJoined = ChannelMember::where('member_id', auth()->id())
+                        ->where('channel_id', $channel->id)
+                        ->exists();
+
+                    return $isJoined || $channel->communities->count() > 0;
+                });
+
             return $channels;
-    
+
         } catch (Exception $e) {
             Log::error('Error while getting channels for authenticated user that is joined in it', ['error' => $e->getMessage(), 'trace' => $e->__toString()]);
             return false;
         }
     }
-    
-    
+
+
 
     public function getArchivedChannels()
     {
         try {
             return ChannelMember::where([
-                        'member_id'     => auth()->id(),
-                        'channel_level' => ChannelLevel::TEAM
-                    ])
-                    ->whereHas('channel', function ($q) {
-                        return $q->where('is_archived', YesOrNo::YES)
-                            ->with(['channel.members', 'joinedCommunities', 'joinedCommunities.channel', 'joinedCommunities.joinedSubCommunities', 'joinedCommunities.joinedSubCommunities.channel']);
-                    })
-                    ->paginate(self::PERPAGE);
+                'member_id' => auth()->id(),
+                'channel_level' => ChannelLevel::TEAM
+            ])
+                ->whereHas('channel', function ($q) {
+                    return $q->where('is_archived', YesOrNo::YES)
+                        ->with(['channel.members', 'joinedCommunities', 'joinedCommunities.channel', 'joinedCommunities.joinedSubCommunities', 'joinedCommunities.joinedSubCommunities.channel']);
+                })
+                ->paginate(self::PERPAGE);
         } catch (Exception $e) {
             Log::error('Error while getting channels for authenticated user that it archived', ['error' => $e->getMessage(), 'trace' => $e->__toString()]);
             return false;
@@ -94,21 +94,21 @@ class ChannelUserService extends Service
     {
         DB::beginTransaction();
         try {
-            foreach(Arr::get($data, 'who_receive_message') as $userId) {
+            foreach (Arr::get($data, 'who_receive_message') as $userId) {
                 $checkFounding = ChatUser::where([
-                                    'who_start_chat'        => auth()->id(),
-                                    'who_receive_message'   => $userId
-                                ])
-                                ->orWhere([
-                                    'who_start_chat'        => $userId,
-                                    'who_receive_message'   => auth()->id()
-                                ])
-                                ->first();
+                    'who_start_chat' => auth()->id(),
+                    'who_receive_message' => $userId
+                ])
+                    ->orWhere([
+                        'who_start_chat' => $userId,
+                        'who_receive_message' => auth()->id()
+                    ])
+                    ->first();
 
-                if(!$checkFounding) {
+                if (!$checkFounding) {
                     ChatUser::create([
-                        'who_start_chat'        => auth()->id(),
-                        'who_receive_message'   => $userId
+                        'who_start_chat' => auth()->id(),
+                        'who_receive_message' => $userId
                     ]);
                 }
             }
@@ -125,16 +125,17 @@ class ChannelUserService extends Service
         try {
             $authUserId = auth()->id();
             return ChatUser::select(
-                    'chat_users.id as chat_user_id',
-                    'users.id as user_id',
-                    'users.name',
-                    'users.phone',
-                    'users.profile_image',
-                    'chat_users.notify_counter',
-                    'chat_users.counter',
-                    'chat_users.created_at',
-                    'chat_users.updated_at',
-                    )
+                'chat_users.id as chat_user_id',
+                'users.id as user_id',
+                'users.name',
+                'users.email',
+                'users.phone',
+                'users.profile_image',
+                'chat_users.notify_counter',
+                'chat_users.counter',
+                'chat_users.created_at',
+                'chat_users.updated_at',
+            )
                 ->join('users', function ($join) use ($authUserId) {
                     $join->on('users.id', '=', DB::raw("
                         CASE
@@ -145,9 +146,9 @@ class ChannelUserService extends Service
                 })
                 ->where(function ($query) use ($authUserId) {
                     $query->where('chat_users.who_start_chat', $authUserId)
-                          ->orWhere('chat_users.who_receive_message', $authUserId);
+                        ->orWhere('chat_users.who_receive_message', $authUserId);
                 })
-                 ->orderBy('chat_users.updated_at','desc')
+                ->orderBy('chat_users.updated_at', 'desc')
                 ->groupBy('users.id')
                 ->paginate(self::PERPAGE);
         } catch (Exception $e) {
